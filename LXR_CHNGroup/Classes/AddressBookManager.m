19b7bd910520571366d13be6d2fb9ec89ada8e93
LXR <1932121@qq.com>
2017-07-30 03:24:44 +0800

    0.2.0

---
+++ LXR_CHNGroup/Classes/AddressBookManager.m
@@ -0,0 +1,312 @@
+//
+//  AddressBookManager.h
+//  Pods
+//
+//  Created by MACBOOK on 2017/7/22.
+//  Copyright © 2017年 LXR. All rights reserved.
+//
+
+#import "AddressBookManager.h"
+#import "PersonModel.h"
+#import "NSString+pinYin.h"
+
+#ifdef __IPHONE_9_0
+#import <Contacts/Contacts.h>
+#endif
+#import <AddressBook/AddressBook.h>
+
+
+
+#define IOS9_LATER ([[UIDevice currentDevice] systemVersion].floatValue > 9.0 ? YES : NO )
+
+@interface AddressBookManager()
+#ifdef __IPHONE_9_0
+/** iOS9之后的通讯录对象*/
+@property (nonatomic, strong) CNContactStore *contactStore;
+#endif
+@end
+
+@implementation AddressBookManager
+LXRSingletonM(Manager)
+
+
+- (void)initialize
+{
+    [self getOrderAddressBook:nil authorizationFailure:nil];
+}
+
+#pragma mark - 获取原始顺序所有联系人
+- (void)getOriginalAddressBook:(AddressBookArrayBlock)addressBookArray authorizationFailure:(AuthorizationFailure)failure
+{
+    // 将耗时操作放到子线程
+    dispatch_queue_t queue = dispatch_queue_create("addressBook.array", DISPATCH_QUEUE_SERIAL);
+    
+    dispatch_async(queue, ^{
+        
+        NSMutableArray *array = [NSMutableArray array];
+        [self getAddressBookDataSource:^(PersonModel *model) {
+            
+            [array addObject:model];
+            
+        } authorizationFailure:^{
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                failure ? failure() : nil;
+            });
+        }];
+        
+        // 将联系人数组回调到主线程
+        dispatch_async(dispatch_get_main_queue(), ^{
+            addressBookArray ? addressBookArray(array) : nil ;
+        });
+    });
+    
+}
+
+#pragma mark - 获取按A~Z顺序排列的所有联系人
+- (void)getOrderAddressBook:(AddressBookDictBlock)addressBookInfo authorizationFailure:(AuthorizationFailure)failure
+{
+    
+    // 将耗时操作放到子线程
+    dispatch_queue_t queue = dispatch_queue_create("addressBook.infoDict", DISPATCH_QUEUE_SERIAL);
+    
+    dispatch_async(queue, ^{
+        
+        NSMutableDictionary *addressBookDict = [NSMutableDictionary dictionary];
+        [self getAddressBookDataSource:^(PersonModel *model) {
+            //获取到姓名的大写首字母
+            NSString *firstLetterString = [NSString getFirstLetterFromString:model.name];
+            //如果该字母对应的联系人模型不为空,则将此联系人模型添加到此数组中
+            if (addressBookDict[firstLetterString])
+            {
+                [addressBookDict[firstLetterString] addObject:model];
+            }
+            //没有出现过该首字母，则在字典中新增一组key-value
+            else
+            {
+                //创建新发可变数组存储该首字母对应的联系人模型
+                NSMutableArray *arrGroupNames = [NSMutableArray arrayWithObject:model];
+                //将首字母-姓名数组作为key-value加入到字典中
+                [addressBookDict setObject:arrGroupNames forKey:firstLetterString];
+            }
+        } authorizationFailure:^{
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                failure ? failure() : nil;
+            });
+        }];
+        
+        // 将addressBookDict字典中的所有Key值进行排序: A~Z
+        NSArray *nameKeys = [[addressBookDict allKeys] sortedArrayUsingSelector:@selector(compare:)];
+        
+        // 将 "#" 排列在 A~Z 的后面
+        if ([nameKeys.firstObject isEqualToString:@"#"])
+        {
+            NSMutableArray *mutableNamekeys = [NSMutableArray arrayWithArray:nameKeys];
+            [mutableNamekeys insertObject:nameKeys.firstObject atIndex:nameKeys.count];
+            [mutableNamekeys removeObjectAtIndex:0];
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                addressBookInfo ? addressBookInfo(addressBookDict,mutableNamekeys) : nil;
+            });
+            return;
+        }
+        
+        // 将排序好的通讯录数据回调到主线程
+        dispatch_async(dispatch_get_main_queue(), ^{
+            addressBookInfo ? addressBookInfo(addressBookDict,nameKeys) : nil;
+        });
+        
+    });
+    
+}
+
+
+
+/**
+ 请求用户是否授权APP访问通讯录的权限
+ @param success 授权成功回调
+ */
+- (void)requestAuthorizationWithSuccessBlock:(void (^)(BOOL isSuccess))success
+{
+    if(IOS9_LATER)
+    {
+#ifdef __IPHONE_9_0
+        // 1.判断是否授权成功,若授权成功直接return
+        if ([CNContactStore authorizationStatusForEntityType:CNEntityTypeContacts] == CNAuthorizationStatusAuthorized)
+        {
+            success(YES);
+            return;
+        }
+        // 2.创建通讯录
+        // 3.授权
+        [self.contactStore requestAccessForEntityType:CNEntityTypeContacts completionHandler:^(BOOL granted, NSError * _Nullable error)
+        {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (granted) {success(YES);}//授权成功
+                else{success(NO);}          //授权失败
+            });
+        }];
+#endif
+    }
+    else
+    {
+        // 1.获取授权的状态
+        ABAuthorizationStatus status = ABAddressBookGetAuthorizationStatus();
+        // 2.判断授权状态,如果是未决定状态,才需要请求
+        if (status == kABAuthorizationStatusNotDetermined) {
+            // 3.创建通讯录进行授权
+            ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+            ABAddressBookRequestAccessWithCompletion(addressBook, ^(bool granted, CFErrorRef error) {
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (granted) {success(YES);}//授权成功
+                    else{success(NO);}          //授权失败
+                });
+            });
+        }
+    }
+}
+
+
+- (void)getAddressBookDataSource:(PPPersonModelBlock)personModel authorizationFailure:(AuthorizationFailure)failure
+{
+    
+    if(IOS9_LATER)
+    {
+        [self getDataSourceFrom_IOS9_Later:personModel authorizationFailure:failure];
+    }
+    else
+    {
+        [self getDataSourceFrom_IOS9_Ago:personModel authorizationFailure:failure];
+    }
+    
+}
+
+#pragma mark - IOS9之前获取通讯录的方法
+- (void)getDataSourceFrom_IOS9_Ago:(PPPersonModelBlock)personModel authorizationFailure:(AuthorizationFailure)failure
+{
+    // 1.获取授权状态
+    ABAuthorizationStatus status = ABAddressBookGetAuthorizationStatus();
+    
+    // 2.如果没有授权,先执行授权失败的block后return
+    if (status != kABAuthorizationStatusAuthorized/** 已经授权*/)
+    {
+        failure ? failure() : nil;
+        return;
+    }
+    
+    // 3.创建通信录对象
+    ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+    
+    //4.按照排序规则从通信录对象中请求所有的联系人,并按姓名属性中的姓(LastName)来排序
+    ABRecordRef recordRef = ABAddressBookCopyDefaultSource(addressBook);
+    CFArrayRef allPeopleArray = ABAddressBookCopyArrayOfAllPeopleInSourceWithSortOrdering(addressBook, recordRef, kABPersonSortByLastName);
+    
+    // 5.遍历每个联系人的信息,并装入模型
+    for(id personInfo in (__bridge NSArray *)allPeopleArray)
+    {
+        PersonModel *model = [PersonModel new];
+        
+        // 5.1获取到联系人
+        ABRecordRef person = (__bridge ABRecordRef)(personInfo);
+        
+        // 5.2获取全名
+        NSString *name = (__bridge_transfer NSString *)ABRecordCopyCompositeName(person);
+        model.name = name.length > 0 ? name : @"无名氏" ;
+        
+        // 5.3获取头像数据
+        NSData *imageData = (__bridge_transfer NSData *)ABPersonCopyImageDataWithFormat(person, kABPersonImageFormatThumbnail);
+        model.headerImage = [UIImage imageWithData:imageData];
+        
+        // 5.4获取每个人所有的电话号码
+        ABMultiValueRef phones = ABRecordCopyValue(person, kABPersonPhoneProperty);
+        
+        CFIndex phoneCount = ABMultiValueGetCount(phones);
+        for (CFIndex i = 0; i < phoneCount; i++)
+        {
+            // 号码
+            NSString *phoneValue = (__bridge_transfer NSString *)ABMultiValueCopyValueAtIndex(phones, i);
+            NSString *mobile = [NSString removeSpecialSubString:phoneValue];
+            
+            [model.mobileArray addObject: mobile ? mobile : @"空号"];
+            
+        }
+        // 5.5将联系人模型回调出去
+        personModel ? personModel(model) : nil;
+        
+        CFRelease(phones);
+    }
+    
+    // 释放不再使用的对象
+    CFRelease(allPeopleArray);
+    CFRelease(recordRef);
+    CFRelease(addressBook);
+    
+}
+
+#pragma mark - IOS9之后获取通讯录的方法
+- (void)getDataSourceFrom_IOS9_Later:(PPPersonModelBlock)personModel authorizationFailure:(AuthorizationFailure)failure
+{
+#ifdef __IPHONE_9_0
+    // 1.获取授权状态
+    CNAuthorizationStatus status = [CNContactStore authorizationStatusForEntityType:CNEntityTypeContacts];
+    // 2.如果没有授权,先执行授权失败的block后return
+    if (status != CNAuthorizationStatusAuthorized)
+    {
+        failure ? failure() : nil;
+        return;
+    }
+    // 3.获取联系人
+    // 3.1.创建联系人仓库
+    // 3.2.创建联系人的请求对象
+    // keys决定能获取联系人哪些信息,例:姓名,电话,头像等
+    NSArray *fetchKeys = @[[CNContactFormatter descriptorForRequiredKeysForStyle:CNContactFormatterStyleFullName],CNContactPhoneNumbersKey,CNContactThumbnailImageDataKey];
+    CNContactFetchRequest *request = [[CNContactFetchRequest alloc] initWithKeysToFetch:fetchKeys];
+    
+    // 3.3.请求联系人
+    [self.contactStore enumerateContactsWithFetchRequest:request error:nil usingBlock:^(CNContact * _Nonnull contact,BOOL * _Nonnull stop) {
+        
+        // 获取联系人全名
+        NSString *name = [CNContactFormatter stringFromContact:contact style:CNContactFormatterStyleFullName];
+        
+        // 创建联系人模型
+        PersonModel *model = [PersonModel new];
+        model.name = name.length > 0 ? name : @"无名氏" ;
+        
+        // 联系人头像
+        model.headerImage = [UIImage imageWithData:contact.thumbnailImageData];
+        
+        // 获取一个人的所有电话号码
+        NSArray *phones = contact.phoneNumbers;
+        
+        for (CNLabeledValue *labelValue in phones)
+        {
+            CNPhoneNumber *phoneNumber = labelValue.value;
+            NSString *mobile = [NSString removeSpecialSubString:phoneNumber.stringValue];
+            [model.mobileArray addObject: mobile ? mobile : @"空号"];
+        }
+        
+        //将联系人模型回调出去
+        personModel ? personModel(model) : nil;
+    }];
+#endif
+    
+}
+
+
+
+#ifdef __IPHONE_9_0
+- (CNContactStore *)contactStore
+{
+    if(!_contactStore)
+    {
+        _contactStore = [[CNContactStore alloc] init];
+    }
+    return _contactStore;
+}
+#endif
+
+
+
+
+@end
